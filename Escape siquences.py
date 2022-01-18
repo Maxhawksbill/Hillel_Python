b619b693f311fe0efa5d6c6c8a22c2af982ccbd9
Maxhawksbill <zabaramax3@gmail.com>
2022-01-18 20:54:12 +0200

    HM2 added

---
+++ Escape siquences.py
@@ -0,0 +1,3 @@
+print('Code\t\tResult\n\\\'\t\tSingle Quote\n\\\t\tBackslash\n\\n\t\tNew Line\n\\r\t\tCarriage Return\n\\t\t\tTab'
+      '\n\\b\t\tBackspace\n\\f\t\tForm Feed'
+      '\n\\ooo\tOctal value\n\\xhh\tHex value')
