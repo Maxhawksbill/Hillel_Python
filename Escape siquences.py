28b8e00261b40b8ffc7ed1850874bef6a02fd09f
Maxhawksbill <zabaramax3@gmail.com>
2022-01-18 21:05:22 +0200

    HM2 changed

--- Escape siquences.py
+++ Escape siquences.py
@@ -1,3 +1,13 @@
-print('Code\t\tResult\n\\\'\t\tSingle Quote\n\\\t\tBackslash\n\\n\t\tNew Line\n\\r\t\tCarriage Return\n\\t\t\tTab'
-      '\n\\b\t\tBackspace\n\\f\t\tForm Feed'
-      '\n\\ooo\tOctal value\n\\xhh\tHex value')
+print('Escape\tSequences\n'
+      '\\\'\t\tSingle Quote\n'
+      '\\\t\tBackslash\n'
+      '\\n\t\tNew Line\n'
+      '\\r\t\tCarriage Return\n'
+      '\\t\t\tTab\n'
+      '\\b\t\tBackspace\n'
+      '\\f\t\tForm Feed\n'
+      '\\a\t\tBell(alert)\n'
+      '\\\'\t\tSingle Quotation mark\n'
+      '\\\"\t\tDouble Quotation mark\n'
+      '\\ooo\tOctal value\n'
+      '\\xhh\tHex value')
