0db7ba4c5b2316ac4eb5c0c8823ebf38811f0bf1
Maxhawksbill <zabaramax3@gmail.com>
2022-01-19 16:11:43 +0200

    New homework

---
+++ HM3/Apples.py
@@ -0,0 +1,10 @@
+schoolmate_qty = input('Type quantity of schoolmates:')
+apple_qty = input('Type quantuty of apples:')
+if int(schoolmate_qty) > int(apple_qty):
+    print('Apples will be enough for:\t'+ apple_qty + ' schoolmate(s)')
+    #print(apple_qty)
+else:
+    sm_apple = int(apple_qty) / int(schoolmate_qty)
+    apple_left = int(apple_qty) % int(schoolmate_qty)
+    print('Each schoolmate receive:\t' + str(int(sm_apple)) + ' apple(s)')
+    print('Apples left in a bucket:\t' + str(int(apple_left)))
