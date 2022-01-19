2ed9cb2c0e039c3db695266ada7a03c360db65b3
Maxhawksbill <zabaramax3@gmail.com>
2022-01-19 21:33:51 +0200

    changes

--- HM3/Apples.py
+++ HM3/Apples.py
@@ -3,7 +3,7 @@ apple_qty = input('Type quantity of apples:')
 if int(schoolmate_qty) == 0:
     print('You do not have schoolmates to share with!')
 elif int(schoolmate_qty) > int(apple_qty):
-    print('Apples will be enough for:\t'+ apple_qty + ' schoolmate(s)')
+    print('Apples will be enough ONLY for:\t'+ apple_qty + ' schoolmate(s) out of ' + schoolmate_qty)
 else:
     sm_apple = int(apple_qty) / int(schoolmate_qty)
     apple_left = int(apple_qty) % int(schoolmate_qty)
