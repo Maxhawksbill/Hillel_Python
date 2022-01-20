de38c275edb52e86faf847b54436bf866e4d5826
Maxhawksbill <zabaramax3@gmail.com>
2022-01-20 16:56:03 +0200

    New HM

---
+++ HM5/reverse_3_digit_number.py
@@ -0,0 +1,10 @@
+number = input('Type any number that equal or greater then 100:')
+number = int(number)
+if number // 100 == 0:
+    print('Please, enter equal or greater then 100!')
+else:
+    digit1 = number // 100
+    digit2 = (number // 10) % 10
+    digit3 = (number % 100) % 10
+    number = digit3 * 100 + digit2 * 10 + digit1
+    print(number, type(number))
