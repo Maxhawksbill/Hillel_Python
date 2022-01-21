c1c1038a1adb5911ad543e2448c46e6c03af748f
Maxhawksbill <97825449+Maxhawksbill@users.noreply.github.com>
2022-01-21 11:41:23 +0200

    Update reverse_3_digit_number.py

--- HM5/reverse_3_digit_number.py
+++ HM5/reverse_3_digit_number.py
@@ -1,4 +1,4 @@
-number = input('Type any number that equal or greater then 100:')
+number = input('Type any number that equal or greater than 100:')
 number = int(number)
 if number // 100 == 0:
     print('Please, enter equal or greater then 100!')
