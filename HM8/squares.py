70b2f95893b2e2db7a1af5ba1d07cd7e045a7e6a
Maxhawksbill <zabaramax3@gmail.com>
2022-01-28 16:23:04 +0200

    changes

--- HM8/squares.py
+++ HM8/squares.py
@@ -1,4 +1,11 @@
 a = int(input('Enter an integer N:'))
+while a == 0:
+    a = int(input('Enter greater than zero!\n'
+              'Enter an integer N:'))
+    if a == 0:
+        continue
+if a == 1:
+    print(a)
 i = 1
 while i**2 < a:
     print(i**2, end=' ')
