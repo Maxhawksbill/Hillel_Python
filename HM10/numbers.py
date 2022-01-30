1a1941738fd6391293d2cfdfd69b4443ae373aaf
Maxhawksbill <zabaramax3@gmail.com>
2022-01-30 12:40:01 +0200

    changes

--- HM10/numbers.py
+++ HM10/numbers.py
@@ -1,13 +1,19 @@
-qty = 0
-sum = 0
-average = 0
+first_val = int(input('Enter an integer number:'))
+if first_val == 0:
+    exit(0)
+qty = 1
+sum = first_val
+average = first_val
 odds = 0
 evens = 0
-numbers = []
-maximum = minimum = 0
+if (first_val % 2) == 0:
+    evens = evens + 1
+else:
+    odds = odds + 1
+prev_val = first_val
+maximum = minimum = first_val
 while True:
     val = int(input('Enter an integer number:'))
-    numbers.insert(0, val)
     if val == 0:
         break
     qty = qty + 1
@@ -17,13 +23,13 @@ while True:
         evens = evens + 1
     else:
         odds = odds + 1
-    for _ in numbers:
-        maximum = max(numbers)
-        minimum = min(numbers)
+    maximum = max(prev_val, val, first_val)
+    minimum = min(prev_val, val, first_val)
+    prev_val = val
 print('Quantity of numbers: ' + str(int(qty)) + '\n'
-        'Sum of numbers: ' + str(int(sum)) + '\n'
-        'Average of numbers: ' + str(average) + '\n'
-        'Maximum from numbers: ' + str(int(maximum)) + '\n'
-        'Minimum from numbers: ' + str(int(minimum)) + '\n'
-        'Quantity of odd numbers: ' + str(int(odds)) + '\n'
-        'Quantity of even numbers: ' + str(int(evens)))
+    'Sum of numbers: ' + str(int(sum)) + '\n'
+    'Average of numbers: ' + str(average) + '\n'
+    'Maximum from numbers: ' + str(int(maximum)) + '\n'
+    'Minimum from numbers: ' + str(int(minimum)) + '\n'
+    'Quantity of odd numbers: ' + str(int(odds)) + '\n'
+    'Quantity of even numbers: ' + str(int(evens)))
