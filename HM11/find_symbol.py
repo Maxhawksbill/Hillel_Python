26cc954668fefc75d700ee32a560ca8b7cde68e1
Maxhawksbill <zabaramax3@gmail.com>
2022-01-31 19:20:33 +0200

    New HM

---
+++ HM11/find_symbol.py
@@ -0,0 +1,15 @@
+sentence = input('Type a sentence:')
+sym_to_find = input('Type a symbol to find in your sentence:')
+ind = sentence.find(sym_to_find)
+if ind == -1:
+    print('There are no such symbols in your sentence!')
+else:
+    t = 0
+    shift_ind = 0
+    while ind != -1:
+        ind = sentence.find(sym_to_find, shift_ind)
+        if ind == -1:
+            break
+        shift_ind = ind + 1
+        t = t + 1
+    print('Your symbol was found ' + str(t) + ' time (s)')
