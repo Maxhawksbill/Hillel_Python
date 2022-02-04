9a0aa3be5773ee08b7d08e3461002c0afa065cca
Maxhawksbill <zabaramax3@gmail.com>
2022-02-04 18:57:37 +0200

    changes

--- HM15/neighbours.py
+++ HM15/neighbours.py
@@ -1,7 +1,5 @@
 s = input('Enter through the space button a list of integer numbers:')
-print(s)
 lst = list(s.split(' '))
-print(lst)
 t = 0
 for i in range(1, len(lst) - 1):
     if int(lst[i]) > int(lst[i -1]) + int(lst[i + 1]):
