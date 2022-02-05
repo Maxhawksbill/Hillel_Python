05fa8628b94b4084337f873073af915cdcb77278
Maxhawksbill <zabaramax3@gmail.com>
2022-02-05 17:36:35 +0200

    changes

--- HM13/sym_replace.py
+++ HM13/sym_replace.py
@@ -1,8 +1,16 @@
 sentence = input('Type a sentence:')
 sym_to_find = input('Type a symbol for replacement in your sentence:')
 ind = sentence.find(sym_to_find)
+ind_first = sentence.find(sym_to_find)
+shift_ind = sentence.find(sym_to_find)
 if ind == -1:
        print('There are no such symbols in your sentence!')
 else:
-    new_sentence = sentence[0] + sentence[1: len(sentence)-1].replace(sym_to_find, sym_to_find.upper()) + sentence[len(sentence)-1]
+    while ind != -1:
+        ind = sentence.find(sym_to_find, shift_ind)
+        if ind == -1:
+            break
+        shift_ind = ind + 1
+    new_sentence = sentence[0: ind_first + 1] + sentence[ind_first + 1: shift_ind - 1].replace(sym_to_find, sym_to_find.upper())\
+               + sentence[shift_ind - 1: len(sentence)]
     print(new_sentence)
