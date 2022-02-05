964de690ad73075266a761372b78c5898b275ec1
Maxhawksbill <zabaramax3@gmail.com>
2022-02-05 19:20:10 +0200

    changes

--- HM15/neighbours.py
+++ HM15/neighbours.py
@@ -2,8 +2,8 @@ s = input('Enter through the space button a list of integer numbers:')
 lst = list(s.split(' '))
 t = 0
 for i in range(1, len(lst) - 1):
-    if int(lst[i]) > int(lst[i -1]) + int(lst[i + 1]):
+    if (int(lst[i]) > int(lst[i -1])) and (int(lst[i]) > int(lst[i + 1])):
         t = t + 1
     else:
         continue
-print("Quantity of numbers that are greater than it's neigdbours is: " + str(t))
+print("The quantity of numbers which are greater than their neighbors is: " + str(t))
