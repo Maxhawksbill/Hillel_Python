d6365d80e17376630f5768d11d3dc71e6eb5f8c7
Maxhawksbill <zabaramax3@gmail.com>
2022-02-05 19:11:24 +0200

    changes

--- HM14/list_reverse.py
+++ HM14/list_reverse.py
@@ -1,6 +1,8 @@
 s = input('Enter through the space button a list of integer numbers:')
 lst = list(s.split(' '))
-i = 1
-for _ in lst:
-    print(lst[len(lst) - i], end=' ')
-    i = i + 1
+for i in range(0, len(lst) // 2):
+    lst.append(lst[i])
+    lst[i] = lst[len(lst) - 2 - i]
+    lst[len(lst)- 2 - i] = lst[len(lst) - 1]
+    lst.pop()
+print(lst)
