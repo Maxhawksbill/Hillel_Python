ba0c7f139658cd9c3fbf41168d8feb89bc237631
Maxhawksbill <zabaramax3@gmail.com>
2022-02-07 18:33:13 +0200

    changes

--- HM18/unique_items_in_sets.py
+++ HM18/unique_items_in_sets.py
@@ -4,4 +4,4 @@ lst1 = [randint(1, 100) for _ in range(10)]
 lst2 = [randint(1, 100) for _ in range(10)]
 print(lst1)
 print(lst2)
-print('In this list there is (are):', len(set(lst1 + lst2)), ' unique item (s)')
+print('In these lists there is (are):', len(set(lst1 + lst2)), ' unique item (s)')
