d73ea2cf6bd81d30bb88a09b4b24ff74fe14e4c2
Maxhawksbill <zabaramax3@gmail.com>
2022-02-07 22:46:28 +0200

    changes

--- HM19/triangle.py
+++ HM19/triangle.py
@@ -1,9 +1,6 @@
 height = int(input('Enter the height of the triangle: '))
-side = int((3 ** 0.5) * height)
-if 8 > height >= 4:
-    side += 1
-elif height >= 8:
-    side += 2
+a = height // 4
+side = int((3 ** 0.5) * height) + a
 for i in range(height):
     for j in range(side):
         if abs(j - (side // 2)) == i or i == height - 1:
