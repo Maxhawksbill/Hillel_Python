9ef8422bb0b1778e54c30c81b4aaf2c92cd6007b
Maxhawksbill <zabaramax3@gmail.com>
2022-02-08 22:11:49 +0200

    New HM

---
+++ HM22/rhombus_christ.py
@@ -0,0 +1,12 @@
+height = int(input('Enter the height of the rhombus (must be odd number): '))
+side = int((3 ** 0.5) * height)
+for i in range(height):
+    for j in range(side):
+        if i <= height // 2 and j == side // 2 - i:
+            print('* ' * (2 * i + 1), end='')
+        elif ((height // 2) < i and height - i - 1 == abs(j - (side // 2))) or (j == side // 2 and (height // 2) < i):
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
+print()
