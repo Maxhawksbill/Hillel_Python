5c774b000fdf21475ecba3e92984b2cfceaf1eff
Maxhawksbill <zabaramax3@gmail.com>
2022-02-11 14:19:14 +0200

    New HM

---
+++ HM23/dictionaries.py
@@ -0,0 +1,15 @@
+def rep_word(sentence1, sentence2=''):
+    sentence1 = sentence1.split()
+    sentence2 = sentence2.split()
+    d = {i: (sentence1 + sentence2).count(i) for i in (sentence1 + sentence2)}
+    m_value = max(d.values())
+    if m_value == 1:
+        return "There is no common words"
+    else:
+        word = [word for word in d.keys() if d[word] == m_value]
+    return word[len(word) - 1]
+
+
+st1 = input('Enter a sentence: ')
+st2 = input('Enter a second sentence: ')
+print(rep_word(st1, st2))
