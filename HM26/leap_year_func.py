da9b5eabffaea900fd7e0af5f21217ed5812fc11
Maxhawksbill <zabaramax3@gmail.com>
2022-02-11 19:22:01 +0200

    changes

--- HM26/leap_year_func.py
+++ HM26/leap_year_func.py
@@ -1,11 +1,11 @@
 def leap_year(year):
     year = int(year)
     if year % 4 != 0:
-        return f"{str(year)} is not a leap year!"
+        return False #"{str(year)} is not a leap year!"
     elif year % 100 == 0 and year % 400 != 0:
-        return f"{str(year)} is not a leap year!"
+        return False #"{str(year)} is not a leap year!"
     else:
-        return f"{str(year)} is a leap year!"
+        return True #"{str(year)} is a leap year!"
 
 
 print(leap_year(input('Type any year to check if it is leap:')))
