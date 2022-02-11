fc934129afb283e2bebf8ef3f9de9e2a7eeae499
Maxhawksbill <zabaramax3@gmail.com>
2022-02-11 19:01:13 +0200

    changes

--- HM25/season.py
+++ HM25/season.py
@@ -5,7 +5,7 @@ def season(month):
              summer=[6, 7, 8],
              autumn=[9, 10, 11]
              )
-    cur_season = [cur_season for cur_season, cur_month in d.items() if month in d[cur_season]][0].capitalize()
+    cur_season = [cur_season for cur_season in d.keys() if month in d[cur_season]][0].capitalize()
     return f"Current season is: {cur_season}"
 
 
