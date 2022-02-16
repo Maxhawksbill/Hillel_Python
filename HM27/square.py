ca7d9a9dc1af417a58b28b4206b912936e3d9670
Maxhawksbill <zabaramax3@gmail.com>
2022-02-16 10:13:48 +0200

    changes

--- HM27/square.py
+++ HM27/square.py
@@ -3,9 +3,10 @@ def square(side):
     perimeter = side * 4
     sqr = side ** 2
     dgl = (2 * side ** 2) ** 0.5
-    return f"Perimeter of the square: {perimeter}\n" \
-           f"Area of the square: {sqr}\n" \
-           f"Diagonal of the square: {round(dgl, 2)}"
+    return perimeter, sqr, dgl
 
 
-print(square(input('Enter the side of the square to know it\'s perimeter, area and diagonal: ')))
+x, y, z = square(input('Enter the side of the square to know it\'s perimeter, area and diagonal: '))
+print(f'Perimeter of the square: {x}\n'
+      f'Area of the square: {y}\n'
+      f'Diagonal of the square: {round(z, 2)}')
