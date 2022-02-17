88247f765623bcceac2b07938ae0ee4971f1cc41
Maxhawksbill <zabaramax3@gmail.com>
2022-02-17 15:26:51 +0200

    changes

--- HM28/accountant_book.py
+++ HM28/accountant_book.py
@@ -3,8 +3,13 @@ book = [[34587, 'Learning Python, Mark Lutz', 4, 40.95],
         [77226, 'Head First Python, Paul Barry', 3, 32.95],
         [88112, 'Einfuhrung in Python3, Bernd Klein', 3, 24.99]]
 
-order = list(zip(list(map(lambda order_no: order_no[0], book)),
-                 list(map(lambda ammount: round((ammount[2] * ammount[3]), 2) if ammount[2] * ammount[
-                     3] > 100 else round((ammount[2] * ammount[3] + 10), 2), book))))
-print(order)
+# order = list(zip(list(map(lambda order_no: order_no[0], book)),
+#                  list(map(lambda amount: round((amount[2] * amount[3]), 2) if amount[2] * amount[
+#                      3] > 100 else round((amount[2] * amount[3] + 10), 2), book))))
 
+order = list(map(lambda order_no: (order_no[0],) +
+                                  (round(order_no[2] * order_no[3], 2)
+                                   if round(order_no[2] * order_no[3], 2) > 100
+                                   else round(order_no[2] * order_no[3] + 10, 2),), book))
+
+print(order)
