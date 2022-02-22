0216a9a9988ea18265539ab67a37230abfad15d6
Maxhawksbill <zabaramax3@gmail.com>
2022-02-22 16:05:07 +0200

    changes

--- HM31/number_converter.py
+++ HM31/number_converter.py
@@ -8,9 +8,10 @@ def number_converter(number, scale=2):
     while number > 0:
         num_conv += [str(number % scale) if number % scale in range(0, 10) else ascii_uppercase[number % scale - 10]]
         number = number // scale
-    for i in num_conv[::-1]:
-        res += i
-    return res
+    # for i in num_conv[::-1]:
+    #     res += i
+    res2 = ''.join(num_conv[::-1])
+    return res2
 
 
 print(number_converter(input("Enter a number to convert: "), input("Enter a scale to convert to: ")))
