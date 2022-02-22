2eaffdf6bd838b9e716bf5775b9befd89ccfa6a8
Maxhawksbill <97825449+Maxhawksbill@users.noreply.github.com>
2022-02-22 13:34:51 +0200

    Update dict_generator.py

--- HM30/dict_generator.py
+++ HM30/dict_generator.py
@@ -6,5 +6,5 @@ def symbol(sym_number):
     return dict.get(sym_number)
 
 
-print(symbol(input("Enter a number in interval from 32 to 127 including: ")))
+print(symbol(input("Enter a number in interval from 32 to 127 included: ")))
 print(dict)
