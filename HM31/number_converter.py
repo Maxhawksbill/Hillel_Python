dba454a90396ee9a63b17d41bb98f6e7b5dd7461
Maxhawksbill <zabaramax3@gmail.com>
2022-02-22 15:53:26 +0200

    changes

--- HM31/number_converter.py
+++ HM31/number_converter.py
@@ -3,11 +3,14 @@ from string import ascii_uppercase
 
 def number_converter(number, scale=2):
     number, scale = int(number), int(scale)
-    num_conv = ''
+    num_conv = []
+    res = ''
     while number > 0:
-        num_conv += str(number % scale) if number % scale in range(0, 10) else ascii_uppercase[number % scale - 10]
+        num_conv += [str(number % scale) if number % scale in range(0, 10) else ascii_uppercase[number % scale - 10]]
         number = number // scale
-    return num_conv[::-1]
+    for i in num_conv[::-1]:
+        res += i
+    return res
 
 
 print(number_converter(input("Enter a number to convert: "), input("Enter a scale to convert to: ")))
