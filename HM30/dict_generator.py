2dff6c8785ce42a2e2c50b92a25916effc99d253
Maxhawksbill <zabaramax3@gmail.com>
2022-02-22 13:28:52 +0200

    New HM

---
+++ HM30/dict_generator.py
@@ -0,0 +1,10 @@
+dict = {key: chr(key) for key in range(32, 127)}
+
+
+def symbol(sym_number):
+    sym_number = int(sym_number)
+    return dict.get(sym_number)
+
+
+print(symbol(input("Enter a number in interval from 32 to 127 including: ")))
+print(dict)
