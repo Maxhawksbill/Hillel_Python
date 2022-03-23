d4c4bd574dcfac6db1620e37faa616100349422f
Maxhawksbill <zabaramax3@gmail.com>
2022-03-23 18:02:01 +0200

    New HM

---
+++ HM32/Counter.py
@@ -0,0 +1,26 @@
+class Counter:
+    def __init__(self, initial_value, min_value=0, max_value=100):
+        self.min_value = min_value
+        self.initial_value = initial_value
+        self.max_value = max_value
+
+    def increase(self):
+        if self.min_value > self.initial_value:
+            print('Initial value can not be less than Minimum Value\n'
+                  'Your counter has been reset to:', self.min_value)
+            self.initial_value = self.min_value
+        if self.initial_value > self.max_value:
+            print('Your counter is out of range!')
+            exit(0)
+        else:
+            self.initial_value += 1
+        return self.initial_value
+
+    def output(self):
+        print(self.initial_value)
+
+
+counter1 = Counter(2, 3, 5)
+counter1.output()
+counter1.increase()
+counter1.output()
